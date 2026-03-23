1dc3ff555fb4ad2b0b75c5759d9b4bdb98623059
Kevincs07 <kevin.infant@thecloudside.com>
2026-03-23 13:12:59 +0530

    New change

--- tests/sample.spec.js
+++ tests/sample.spec.js
@@ -1,8 +1,24 @@
-const { test, expect } = require('@playwright/test');
+import { test, expect } from '@playwright/test';
 
-test('First Sample Project', async ({ page }) => {
-    await page.goto('https://www.google.com');
-    // Check that page loaded and search box is visible
-    const searchBox = page.locator('combobox[aria-label="Search"]');
-    await expect(searchBox).toBeVisible();
+test('test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await page.locator('[data-test="checkout"]').click();
+  await page.locator('[data-test="firstName"]').click();
+  await page.locator('[data-test="firstName"]').fill('Test');
+  await page.locator('[data-test="lastName"]').click();
+  await page.locator('[data-test="lastName"]').fill('Qa');
+  await page.locator('[data-test="lastName"]').press('Tab');
+  await page.locator('[data-test="postalCode"]').fill('12345');
+  await page.locator('[data-test="continue"]').click();
+  await page.locator('[data-test="finish"]').click();
+  await page.locator('[data-test="complete-header"]').click();
+  await page.locator('[data-test="complete-text"]').click();
+  await page.locator('[data-test="back-to-products"]').click();
 });
